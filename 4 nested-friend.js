302ca066608509f79231d3fd71a444ce9d90852f
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 17:02:13 +0600

    nested-friend added

--- 4 nested-friend.js
+++ 4 nested-friend.js
@@ -10,3 +10,25 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+const myScore = 74;
+const friendScore = 36;
+if (myScore > 80) {
+    if (friendScore > 80) {
+        console.log("I go to launch with my Friend");
+    }
+    else if (friendScore < 80 && friendScore >= 60) {
+        console.log("Good luck next time");
+    }
+    else if (60 > friendScore && friendScore >= 40) {
+        console.log("I unseen my friend massage");
+    }
+    else if (40 > friendScore) {
+        console.log("I block my Friend");
+    }
+}
+else if (80 > myScore) {
+    console.log("I go to home and doing sad acting");
+}
+else {
+    console.log("Program Terminated");
+}
