ff7e35fe0c162ed09fa00e079e50a922e3e546cd
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 16:54:26 +0600

    Grade_Calculator added

--- 3 Grade_Calculator.js
+++ 3 Grade_Calculator.js
@@ -11,3 +11,19 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+let studentMarks = 48;
+if (studentMarks >= 90 && 100 >= studentMarks) {
+    console.log("You Achive A ");
+}
+else if (studentMarks >= 80 && 89 >= studentMarks) {
+    console.log("You Achieve B");
+}
+else if (studentMarks >= 70 && 79 >= studentMarks) {
+    console.log("You Achieve C");
+}
+else if (studentMarks >= 60 && 69 >= studentMarks) {
+    console.log("Your Achieve D");
+}
+else {
+    console.log("You are fail");
+}
