7e577576ae1a0bcaf290223848739aed8db6e10c
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-05 16:28:58 +0600

    free drink completed

--- 1 Free Drinks.js
+++ 1 Free Drinks.js
@@ -4,3 +4,13 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+const burgerPrice = 399;
+const howMuchBuy = 3;
+const totalBurgerCost = burgerPrice * howMuchBuy;
+const cokePrice = 30;
+if (totalBurgerCost > 500) {
+    console.log("You Get Free coke");
+}
+else {
+    console.log(`You purchase coke with ${cokePrice} BDT`);
+}
